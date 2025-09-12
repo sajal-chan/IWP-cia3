7dd6f59664d78f1574772ba2b8f4c8a4757add60
sajal-chan <sajalchandra124@gmail.com>
2025-09-12 12:25:56 +0530

    change to sort server.js

--- student_routes.js
+++ student_routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Student = require("./student_model");
+const Student = require("./students_model");
 
 // GET all submitted students
 router.get("/students", async (req, res) => {
